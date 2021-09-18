f4eee46a6044a133ee63ed82a3ea59b073d93b46
hotval <9944695+hotval@users.noreply.github.com>
2021-09-18 11:50:45 +0300

    Update index.php

--- app1/image/app/index.php
+++ app1/image/app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-  if (!isset($_ENV["APP_VARIABLE"])) 
+  if (!isset($_ENV["APP_VARIABLE='Valentin' hotval/dofexam-app1-200036973"])) 
   {
     print "Hello, Brave One!\nIt appears that you forgot to initialize the APP_VARIABLE variable ...\n";
   }
